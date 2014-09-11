96df3c5f9dc27ff9cb3269964e6129526c396201
Jasper Arneberg <C16Jasper.Arneberg@usafa.edu>
2014-09-11 00:20:45 -0600

    started O(log n) multiplication

--- main.asm
+++ main.asm
@@ -12,7 +12,7 @@
             .retainrefs                     ; Additionally retain any sections
                                             ; that have references to current
                                             ; section
-program:	.byte		0x22, 0x11, 0x22, 0x22, 0x33, 0x33, 0x08, 0x44, 0x08, 0x22, 0x09, 0x44, 0xff, 0x11, 0xff, 0x44, 0xcc, 0x33, 0x02, 0x33, 0x00, 0x44, 0x33, 0x33, 0x08, 0x55
+program:	.byte		0x11, 0x33, 0x07
 ADD_OP:   	.equ    	0x11
 SUB_OP: 	.equ		0x22
 MUL_OP: 	.equ		0x33
@@ -40,7 +40,6 @@ initialize:
 read2:
 	mov.b	@R8+,		R5					;R5 holds op code
 	mov.b	@R8+,		R7					;R7 holds second operand
-	mov.b	#zero,		R11					;reset R11 register used in multiplication
 
 testOp:
 	cmp.b	#ADD_OP,	R5
@@ -55,29 +54,49 @@ testOp:
 
 addOp:
 	add.b	R7,			R6
-	jnc		write2RAM
-
-	mov.b	#max_val,	R6					;carry flag, set R6 to max value of 255
-	jmp 	write2RAM
+	jc		store_max
+	jmp		write2RAM
 
 subOp:
 	sub		R7,			R6
-	jge 	write2RAM
-
-	mov.b	#zero,		R6					;negative answers not allowed, store 0 instead
+	jn		store_min						;negative answers not allowed, store 0 instead
 	jmp 	write2RAM
 
-mulOp:
-	cmp		#zero,		R7
-	jz		exit_mul_loop					;jump once R7 counts down to 0
-	dec		R7
-	add.b	R6,			R11					;store result in R11 temporarily
-	jnc		mulOp
+;mulOp:										;O(n)
+;	cmp		#zero,		R7
+;	jz		exit_mul_loop					;jump once R7 counts down to 0
+;	dec		R7
+;	add.b	R6,			R11					;store result in R11 temporarily
+;	jnc		mulOp
+;
+;	mov.b	#max_val,	R11					;value now exceeds limit, store 255
+;
 
-	mov.b	#max_val,	R11					;value now exceeds limit, store 255
+mulOp:										;O(log n)
+	tst.b	R7
+	jz		store_min
+	clr		R11								;R11 stores when R7 is odd
+	mov		R6,			R12					;store original value of first operand
+
+	rrc.b	R7								;cut second operand in half before start
+
+mul_loop:
+	tst.b	R7
+	jz		exit_mul_loop
+
+	clrc
+	rlc.b	R6								;double R6
+	jc		store_max
+
+	rrc.b	R7								;cut R7 in half
+	jnc		mul_loop						;R7 was even
+
+	add.b	R12,		R11					;R7 was odd, add in one value of original first operand
+	jmp		mul_loop
 
 exit_mul_loop:
-	mov.b	R11,		R6
+	add.b	R11,		R6					;add carries to total
+	jc		store_max
 	jmp		write2RAM
 
 clrOp:
@@ -86,10 +105,17 @@ clrOp:
 	inc		R9
 	jmp 	read2
 
+store_min:
+	clr		R6
+	jmp		write2RAM
+
+store_max:
+	mov.b	#max_val,	R6					;carry flag, set R6 to max value of 255
+
 write2RAM:
 	mov.b	R6,			0(R9)
 	inc		R9
-	jmp 	read2
+	jmp 	read2							;back to the top
 
 end:
 	jmp		end								;infinite loop
